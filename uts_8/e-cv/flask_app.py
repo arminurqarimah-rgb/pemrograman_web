1bbff873f3e84e1ec2ce652ea21c2ff73eead473
arminurqarimah-rgb <arminurqarimah@gmail.com>
2026-01-17 21:47:43 +0800

    saya tambahkan project uts_8

---
+++ uts_8/e-cv/flask_app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def beranda():
+    return render_template("/pages/beranda.html")
+
+@app.route("/dokumen")
+def dokumen():
+    return render_template("/pages/dokumen.html")
+
+@app.route("/keahlian")
+def keahlian():
+    return render_template("/pages/keahlian.html")
+
+@app.route("/kontak")
+def kontak():
+    return render_template("/pages/kontak.html")
+
+@app.route("/pendidikan")
+def pendidikan():
+    return render_template("/pages/pendidikan.html")
+
+@app.route("/pengalaman")
+def pengalaman():
+    return render_template("/pages/pengalaman.html")
+
+@app.route("/tentang")
+def tentang():
+    return render_template("pages/tentang.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
